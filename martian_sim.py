ebd6a6e6fe9464cec969edc2c8c1e3198bddf5b6
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-01 15:29:44 -0400

    Update martian_sim.py

--- martian_sim.py
+++ martian_sim.py
@@ -6,6 +6,18 @@ screen = pygame.display.set_mode((2048,1024))
 
 clock = pygame.time.Clock()
 
+class Rover:
+  def __init__(self, name, image):
+    self.name = name
+    self.image = image
+
+r1 = Rover("Sixwheeler", "1.jpg")
+r2 = Rover("Camdrone", "2.jpg")
+r3 = Rover("Invertebrae", "3.jpg")
+r4 = Rover("Hopper", "4.jpg")
+r5 = Rover("5", "5.jpg")
+r6 = Rover("6", "6.jpg")
+
 while True:
     # Process player inputs.
     for event in pygame.event.get():
