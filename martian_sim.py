c793b87dd9815a681333156636cbad892d2d703a
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-14 17:50:45 -0400

    Update martian_sim.py

--- martian_sim.py
+++ martian_sim.py
@@ -1,27 +1,18 @@
 import bs4
 
+#"Driver"
+#"Flier"
+#"Slitherer"
+#"Hopper"
+#"Crawler"
+#"Runner"
+
 with open("index.html") as inf:
     txt = inf.read()
     soup = bs4.BeautifulSoup(txt)
 
-new_link = soup.new_tag("link", rel="icon", type="image/png", href="img/tor.png")
-soup.head.append(new_link)
+new_para = soup.new_tag("paragraph", text="  Driver, Flier, Slitherer, Hopper, Crawler, and Runner meet up for the cave exploration mission. The first one drives the other 5 to the cavity, and hoists them down. They find traces of water, and a big place for settlement. Findings reported...")
+soup.head.append(new_para)
 
-with open("existing_file.html", "w") as outf:
+with open("index.html", "w") as outf:
     outf.write(str(soup))
-
-class Rover:
-  def __init__(self, name):
-    self.name = name
-
-r1 = Rover("Driver")
-r2 = Rover("Flier")
-r3 = Rover("Slitherer")
-r4 = Rover("Hopper")
-r5 = Rover("Crawler")
-r6 = Rover("6")
-
-print(getattr(r1,'name'), ", ", getattr(r2,'name'), ", ", getattr(r3,'name'), ", ", getattr(r4,'name'), ", ", getattr(r5,'name'), ", and ", getattr(r6,'name'), " meet up for the cave exploration mission.")
-print("The first one drives the other 5 to the cavity, and hoists them down.")
-print("They find traces of water, and a big place for settlement.")
-print("Findings reported...")
