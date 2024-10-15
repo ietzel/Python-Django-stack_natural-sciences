88af852bfd4d3bbf28d48c15332be9e0ff510a34
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-15 13:37:24 -0400

    Update martian_sim.py

--- martian_sim.py
+++ martian_sim.py
@@ -11,8 +11,7 @@ with open("index.html") as inf:
     txt = inf.read()
     soup = bs4.BeautifulSoup(txt)
 
-new_para = soup.new_tag("paragraph", text="Driver, Flier, Slitherer, Hopper, Crawler, and Runner meet up for the cave exploration mission. The first one drives the other 5 to the cavity, and hoists them down. They find traces of water, and a big place for settlement. Findings reported...")
-soup.head.append(new_para)
+new_para = soup.new_tag("paragraph", text="soup.head.append(new_para)
 
 with open("index.html", "w") as outf:
     outf.write(str(soup))
