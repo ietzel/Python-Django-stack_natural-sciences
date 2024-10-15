d424059a102f6997ecb38b9babbf7029247fd15f
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-15 14:10:55 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,12 @@
 <?php
+
+print "<head>\n";
+print "  <title>Mars Simulator</title>\n";
+print "</head>\n";
+print "<body>\n";
+print "  <p>Driver, Flier, Slitherer, Hopper, Crawler, and Runner meet up for the cave exploration mission. The first one drives the other 5 to the cavity, and hoists them down. They find traces of water, and a big place for settlement. Findings reported...\")</p>\n";
+print "</body>";
+
 $servername = "localhost";
 $username = "username";
 $password = "password";
