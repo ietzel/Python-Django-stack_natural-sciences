62f8825585b6c45deaad563785e97fa016ed6042
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-15 14:07:52 -0400

    Update main.php

--- main.php
+++ main.php
@@ -3,10 +3,8 @@ $servername = "localhost";
 $username = "username";
 $password = "password";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
